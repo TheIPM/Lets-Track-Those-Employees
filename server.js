76e3e6174f24348f81dcfaa0f34d58882e7645e3
TheIPM <islamluse@gmail.com>
2023-03-04 16:08:45 +1030

    moved connection.end outside the while running loop so i can go back with out it the pool closing after every goback function

--- server.js
+++ server.js
@@ -2,18 +2,22 @@ const express = require('express');
 const mysql = require('mysql2/promise');
 const inquirer = require('inquirer');
 
-
 const app = express();
 app.use(express.json());
 
-
-
-const connection = mysql.createPool( {
-    host: 'localhost',
-    user: 'root',
-    password: '4anp488K.',
-    database: 'company_db'
+const connection = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '4anp488K.',
+  database: 'company_db',
 });
+
+const goBackPrompt = {
+  type: 'list',
+  name: 'goBack',
+  message: 'What would you like to do?',
+  choices: ['Return to main menu'],
+};
 
 app.get('/departments', async (req, res) => {
   const [rows] = await connection.query('SELECT id, name FROM department');
@@ -55,9 +59,10 @@ app.put('/employees/:id', async (req, res) => {
   res.sendStatus(204);
 });
 
-
-
 async function main() {
+  let running = true;
+  
+  while (running) {
     const { action } = await inquirer.prompt([
       {
         type: 'list',
@@ -70,88 +75,113 @@ async function main() {
           { name: 'Add a department', value: 'addDepartment' },
           { name: 'Add a role', value: 'addRole' },
           { name: 'Add an employee', value: 'addEmployee' },
-          { name: 'Update an employee role', value: 'updateEmployeeRole' }
-        ]
-      }
+          { name: 'Update an employee role', value: 'updateEmployeeRole' },
+          { name: 'Exit', value: 'exit' },
+        ],
+      },
     ]);
-  
+
     switch (action) {
-        case 'viewDepartments':
-          const [rows] = await connection.execute('SELECT id, name FROM department');
-          console.table(rows);
-          break;
-        case 'viewRoles':
-          const [rows2] = await connection.execute('SELECT role.id, role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id');
-          console.table(rows2);
-          break;
-        case 'viewEmployees':
-          const [rows3] = await connection.execute('SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id');
-          console.table(rows3);
-          break;
-          case 'addDepartment':
-            const { departmentName } = await inquirer.prompt([
+      case 'viewDepartments':
+        const [deptRows] = await connection.execute('SELECT id, name FROM department');
+        console.table(deptRows);
+        await inquirer.prompt(goBackPrompt);
+        break;
+      case 'viewRoles':
+        const [roleRows] = await connection.execute(
+          'SELECT role.id, role.title, role.salary, department.name AS department FROM role JOIN department ON role.department_id = department.id'
+        );
+        console.table(roleRows);
+        await inquirer.prompt(goBackPrompt);
+        break;
+      case 'viewEmployees':
+        const [employeeRows] = await connection.execute(
+          'SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, " ", manager.last_name) AS manager FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id LEFT JOIN employee manager ON employee.manager_id = manager.id'
+        );
+        console.table(employeeRows);
+        await inquirer.prompt(goBackPrompt);
+        break;
+      case 'addDepartment':
+        const { departmentName } = await inquirer.prompt([
           {
             type: 'input',
             name: 'departmentName',
-            message: 'Enter the name of the department:'
-          }
+            message: 'Enter the name of the department:',
+          },
         ]);
         await connection.execute('INSERT INTO department (name) VALUES (?)', [departmentName]);
         console.log(`Department '${departmentName}' added successfully.`);
         break;
-        case 'addRole':
-            const [departmentRowsForRole] = await connection.execute('SELECT id, name FROM department');
-            const { title, salary, department_id } = await inquirer.prompt([
+      case 'addRole':
+        const [deptRowsForRole] = await connection.execute('SELECT id, name FROM department');
+        const { title, salary, department_id } = await inquirer.prompt([
           {
             type: 'input',
             name: 'title',
-            message: 'Enter the title of the role:'
+            message: 'Enter the title of the role:',
           },
           {
             type: 'input',
             name: 'salary',
-            message: 'Enter the salary for the role:'
+            message: 'Enter the salary for the role:',
           },
           {
             type: 'list',
             name: 'department_id',
             message: 'Select the department for the role:',
-            choices: departmentRowsForRole.map(department => ({ name: department.name, value: department.id }))
-          }
+            choices: deptRowsForRole.map((dept) => ({ name: dept.name, value: dept.id })),
+          },
         ]);
-        await connection.execute('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [title, salary, department_id]);
+        await connection.execute('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)', [
+          title,
+          salary,
+          department_id,
+        ]);
         console.log(`Role '${title}' added successfully.`);
         break;
-        case 'addEmployee':
-            const [roleRowsForEmployee] = await connection.execute('SELECT id, title FROM role');
-            const [managerRowsForEmployee] = await connection.execute('SELECT id, first_name, last_name FROM employee');
-            const { first_name, last_name, role_id, manager_id } = await inquirer.prompt([
-              {
-                type: 'input',
-                name: 'first_name',
-                message: 'Enter the first name of the employee:'
-              },
-              {
-                type: 'input',
-                name: 'last_name',
-                message: 'Enter the last name of the employee:'
-              },
-              {
-                type: 'list',
-                name: 'role_id',
-                message: 'Select the role for the employee:',
-                choices: roleRowsForEmployee.map(role => ({ name: role.title, value: role.id }))
-              },
-              {
-                type: 'list',
-                name: 'manager_id',
-                message: 'Select the manager for the employee:',
-                choices: [...managerRowsForEmployee.map(manager => ({ name: `${manager.first_name} ${manager.last_name}`, value: manager.id })), { name: 'None', value: null }]
-              }
-            ]);
-            await connection.execute('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [first_name, last_name, role_id, manager_id]);
-      console.log(`Employee '${first_name} ${last_name}' added successfully.`);
-      break;
+      case 'addEmployee':
+        while (true) {
+          const [roleRowsForEmployee] = await connection.execute('SELECT id, title FROM role');
+          const [managerRowsForEmployee] = await connection.execute(
+            'SELECT id, first_name, last_name FROM employee WHERE manager_id IS NULL'
+          );
+          const { first_name, last_name, role_id, manager_id, goBack } = await inquirer.prompt([
+            {
+              type: 'input',
+              name: 'first_name',
+              message: 'Enter the first name of the employee:'
+            },
+            {
+              type: 'input',
+              name: 'last_name',
+              message: 'Enter the last name of the employee:'
+            },
+            {
+              type: 'list',
+              name: 'role_id',
+              message: 'Select the role for the employee:',
+              choices: roleRowsForEmployee.map(role => ({ name: role.title, value: role.id }))
+            },
+            {
+              type: 'list',
+              name: 'manager_id',
+              message: 'Select the manager for the employee:',
+              choices: [...managerRowsForEmployee.map(manager => ({ name: `${manager.first_name} ${manager.last_name}`, value: manager.id })), { name: 'None', value: null }]
+            },
+            goBackPrompt,
+          ]);
+    
+          if (goBack === 'Go back') {
+            break;
+          } else if (goBack === 'Return to main menu') {
+            running = false;
+            break;
+          }
+    
+          await connection.execute('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)', [first_name, last_name, role_id, manager_id]);
+          console.log(`Employee '${first_name} ${last_name}' added successfully.`);
+        }
+        break;
       case 'updateEmployeeRole':
         const [employeeRowsForUpdate] = await connection.execute('SELECT id, CONCAT(first_name, " ", last_name) AS name FROM employee');
         const [roleRowsForUpdate] = await connection.execute('SELECT id, title FROM role');
@@ -172,18 +202,26 @@ async function main() {
         await connection.execute('UPDATE employee SET role_id = ? WHERE id = ?', [new_role_id, employee_id]);
         console.log(`Employee updated successfully.`);
         break;
-          default:
-            console.error(`Invalid action: ${action}`);
-            break;
-        }
-        
-        await connection.end();}
     
-    main().catch(err => {
-      console.error(err);
-      connection.end();
-    });
-
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
-});
+      case 'exit':
+        running = false;
+        break;
+    
+     
+        default:
+          console.error(`Invalid action: ${action}`);
+          break;
+      }
+    }
+  
+    await connection.end();
+  }
+  
+  main().catch((err) => {
+    console.error(err);
+    connection.end();
+  });
+  
+  app.listen(3000, () => {
+    console.log('Server listening on port 3000');
+  });
